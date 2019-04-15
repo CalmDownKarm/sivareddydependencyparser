6edc1dec837dc07daf095456641ee5d93ff5417c
calmdownkarm <karmanyaaggarwal@gmail.com>
2019-04-15 11:06:34 +0530

    updates

--- lib/dependency_parser.py
+++ lib/dependency_parser.py
@@ -1,4 +1,22 @@
-import regex_strings
+import regex_strings as rs
 
-def run_tokenize(language=None, encoding='utf-8', glue=False, stream=False):
-    ...
+
+def tokenise(data, lsd, glue):
+    '''
+    '''
+
+
+def run_tokenize(data, language='English', encoding='utf-8', glue='<g/>', stream=False, quiet=False):
+    '''
+    passed a language, an encoding, and some text runs the tokenizer
+    :param language: string, language of the text
+    :param encoding: string, encoding type
+    :param glue: boolean, use glue character
+    :param stream: boolean, not sure
+    returns newline seperated list of tokens
+    '''
+    # no_glue  = False
+    lsd = [dict_.get(language, dict_.get('default')) for dict_ in [rs.clictics, rs.abbreviations, rs.word]]
+    # lsd = LanguageData()
+    tokens = '\n'.join(tokenise(data, lsd, glue))
+    return tokens
