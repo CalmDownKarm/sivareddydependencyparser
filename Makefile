711fb32f907c3975f30c3170f96cea937eb5530d
karmanya.aggarwal <karmanya.aggarwal@gramener.com>
2019-04-25 23:11:13 +0530

    fix conll

--- Makefile
+++ Makefile
@@ -14,11 +14,10 @@ TOKENIZER=python2 ./bin/unitok.py -l hindi -n
 %.output: %.input.txt
 	# uncomment below line if you require a normalizer
 	cat $< | $(TOKENIZER) | sed -e 's/।/./g' | sed -e 's/^\.$$/.\n<\/s>\n<s>/g' |  $(NORMALIZE)  > $@.tmp.words
-	$(TAGGER) $@.tmp.words  | sed -e 's/\t\+/\t/g' > $@.tmp.tags.expectedlemma
 	# # uncomment below line if you do not require a normalizer
 	# cat $< | $(TOKENIZER) |  sed -e 's/।/./g' | sed -e 's/^\.$$/.\n<\/s>\n<s>/g'  > $@.tmp.words
-	# $(TAGGER) $@.tmp.words | sed -e 's/\t\+/\t/g' | $(LEMMATIZER) | $(TAG2VERT) | $(POSMOD) | cut -f1,2,3 > $@.tmp.tag
-	# python2 bin/convert_format.py $@.tmp.tag $@.tmp.tag.conll
+	$(TAGGER) $@.tmp.words | sed -e 's/\t\+/\t/g' | $(LEMMATIZER) | $(TAG2VERT) | $(POSMOD) | cut -f1,2,3 > $@.tmp.tag
+	python2 bin/convert_format.py $@.tmp.tag $@.tmp.tag.conll
 	# java -jar bin/malt.jar -c test_complete -i $@.tmp.tag.conll -o $@.tmp.output -m parse
 	# python2 bin/convert_output.py $@.tmp.output $@
 	# rm  *.tmp.*
--- refactor/dependency_parser.py
+++ refactor/dependency_parser.py
@@ -1,4 +1,5 @@
 import re
+import json
 import html
 import regex_strings as rs
 
@@ -77,6 +78,7 @@ def normalize(tokens):
     }
     return [''.join(map(lambda x: vowels_to_replace.get(x, x), token)) for token in tokens]
 
+
 def get_pos_tags(filepath):
     '''The POS Tagger that Sivareddy uses is a something binary that takes input from files
     Not sure if it can take input straight from memory
@@ -85,22 +87,110 @@ def get_pos_tags(filepath):
     '''
     import os
     import subprocess
-    args= [
+    args = [
         './hindi-pos-tagger/bin/tnt',
         '-v0',
         '-H',
         'hindi-pos-tagger/models/hindi',
         filepath
     ]
-    sub = subprocess.run(args=args, capture_output=True, cwd=os.path.abspath('..'))
+    sub = subprocess.run(args=args, capture_output=True,
+                         cwd=os.path.abspath('..'))
     tags = sub.stdout.decode('utf-8').split('\n')
     multi_tabs = re.compile(r'\t+')
     return [multi_tabs.sub('\t', tokens) for tokens in tags]
 
-def lemmatise():
-    '''given a list of tokens and POS tags, lemmatizes them'''
-    ...
+
+def lemmatise(tagged_tokens, lemma):
+    '''given a list of tokens and POS tags, lemmatizes them
+    :param lemma: string, filepath of the lemma or filepath of lemma stored as a JSON
+    expects input to be 2 columns, word\ttag
+    :param text: list of strings to lemmatise.
+    '''
+    if isinstance(lemma, str):
+        with open(lemma) as f:
+            lemma = json.load(f)
+    return [lemmatise_helper(line, lemma) for line in tagged_tokens]
 
 
+def lemmatise_helper(line, lemma):
+        line = line.strip()  # Could probably remove this
+        if not line or line.startswith('<'):
+            return line
+        else:
+            cols = line.split()
+            if len(cols) != 2:
+                return line
+            else:
+                if cols[0] in lemma and cols[1] in lemma[cols[0]]:
+                    return "%s\t%s" % (line, lemma[cols[0]][cols[1]])
+                else:
+                    return "%s\t%s" % (line, cols[0]+".")
 
 
+def tag2letter(tag):
+    if re.match("NEG", tag):
+        return "x"
+    elif re.match("^[JNV]", tag):
+       return tag[0].lower()
+    elif re.match("RB", tag):
+       return "r"
+    elif re.match("PRP", tag):
+       return "d"
+    elif re.match("PSP", tag):
+       return "p"
+    elif re.match("XC", tag):
+        return "n"
+    elif re.match("CC", tag):
+       return "c"
+    elif re.match("INJ", tag):
+       return "i"
+    else:
+       return "x"
+
+
+def tag2vert(lemmatized_tokens):
+    return [tag2vert_helper(line) for line in lemmatized_tokens]
+
+
+def tag2vert_helper(line):
+    '''Takes the output of the lemmatizer and creates vertical columns for some reason.'''
+    if line == "" or line[0] == "<":
+        return line
+    else:
+        fields = line.split("\t")
+        if len(fields) == 3:
+            word = fields[0]
+            tag = re.findall(r"^(.*)\.(.*?)\.(.*?)\.(.*?)\.(.*?)\.(.*?)$", fields[1])
+            if tag == []:
+               tagSplit = ['UNK', '', '', '', '', '']
+            else:
+               tagSplit = tag[0]
+            lemma, suffix = re.findall(r"^(.*)\.(.*?)$", fields[2])[0]
+            if lemma == "":
+                lemma = word
+            lempos = lemma + "-" + tag2letter(tagSplit[0])
+            return "%s\t%s\t%s\t%s\t%s\n" % (word, tagSplit[0], lempos, suffix, "\t".join(tagSplit[1:]))
+        else:
+            return line
+
+def modify_pos_tags(vert_tags):
+    '''
+    I honestly don't know what this function does, or why it's needed, but it's here.
+    '''
+    return [modify_pos_tags_helper(line) for line in vert_tags]
+
+def modify_pos_tags_helper(line):
+    if line.startswith('<'):
+        return line
+    cols= line[:-1].split('\t')
+    if len(cols)<2:
+        return line
+    if cols[1] in ["PSP", "CC"]:
+        cols[1]+= ":" + cols[2][:-2]
+    elif cols[1]=="SYM":
+        if cols[0]=="।":
+            cols[1]="."
+        else:
+            cols[1]= cols[0]
+    return '\t'.join(cols)
